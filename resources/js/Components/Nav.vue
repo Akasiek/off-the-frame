03693f8a9e6660ee15187bdb1c4f398e466b58cb
Kamil Pomykała <kamil.pomykala@upware.pl>
2024-04-06 19:49:31 +0200

    feat: Home, Nav and Footer

---
+++ resources/js/Components/Nav.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import { Link } from '@inertiajs/vue3';
+import Logo from '@/Components/Logo.vue';
+
+const links = [
+  {
+    // route: 'style-guides',
+    route: 'home',
+    name: 'style guides',
+  },
+  {
+    // route: 'products',
+    route: 'home',
+    name: 'products by category',
+  },
+  {
+    // route: 'outfit-of-the-day',
+    route: 'home',
+    name: 'outfit of the day',
+  },
+];
+</script>
+
+<template>
+  <nav
+    class="sticky top-0 inset-x-0 flex flex-col items-center gap-y-4 py-4 rounded-b-3xl shadow-[0_4px_16px_0_rgba(0,0,0,0.075)] bg-white-100 z-50"
+  >
+    <div>
+      <Link :href="route('home')" class="font-semibold">
+        <Logo class="h-16" />
+      </Link>
+    </div>
+
+    <div class="flex gap-x-6 ml-8">
+      <Link :href="route(link.route)" v-for="link in links" :key="link.route" class="font-semibold text-black cool-underline">
+        {{ link.name }}
+      </Link>
+    </div>
+    <div class="bg-white-100 hover:bg-zinc-200 transition-colors absolute right-0 w-4 h-4 rounded-full">
+      <Link :href="route('dashboard')" class="block w-4 h-4" />
+    </div>
+  </nav>
+</template>
---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { Head } from '@inertiajs/vue3';
+import PrimaryLayout from '@/Layouts/PrimaryLayout.vue';
+</script>
+
+<template>
+  <Head title="Welcome" />
+
+  <PrimaryLayout>
+    <h1>Home!</h1>
+  </PrimaryLayout>
+</template>
