c896fe55d079856c67eb5de770cf5152b90031b1
Kamil Pomykała <kamil.pomykala@upware.pl>
2024-04-06 19:48:31 +0200

    refactor: Removed unnecessary routes and updated font dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,33 +3,21 @@ import forms from '@tailwindcss/forms';
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-    './storage/framework/views/*.php',
-    './resources/views/**/*.blade.php',
-    './resources/js/**/*.vue',
-  ],
+    content: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+        './resources/js/**/*.vue',
+    ],
 
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Montserrat', ...defaultTheme.fontFamily.sans],
-        serif: ['Lora', ...defaultTheme.fontFamily.serif],
-      },
-      colors: {
-        black: {
-          DEFAULT: '#3B3A3A',
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Montserrat', ...defaultTheme.fontFamily.sans],
+                serif: ['Lora', ...defaultTheme.fontFamily.serif],
+            },
         },
-        white: {
-          50: '#FBFBFB',
-          100: '#FAF9F9',
-        },
-        red: {
-          DEFAULT: '#7A232F',
-        },
-      },
     },
-  },
 
-  plugins: [forms],
+    plugins: [forms],
 };
