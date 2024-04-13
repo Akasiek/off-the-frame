b15d99a3301627447285b1e7b70e03d809a502de
Kamil Pomykała <kamil.pomykala@upware.pl>
2024-04-13 21:49:14 +0200

    feat: Add interfaces ProductLink and ProductCategory
    Added interfaces ProductLink and ProductCategory to better define
    product and category objects used in the application. This will
    improve type safety and code readability.

---
+++ resources/js/Interfaces/ProductLink.ts
@@ -0,0 +1,7 @@
+export interface ProductLink {
+  id: number;
+  url: string;
+  price: number;
+  store: string;
+  product_id: number;
+}
---
+++ resources/js/Pages/Product/Dashboard.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import DashboardLayout from '@/Layouts/DashboardLayout.vue';
+import { Head } from '@inertiajs/vue3';
+import Table from '@/Components/Product/Dashboard/Table.vue';
+import { Product, ProductCategory } from '@/Interfaces';
+
+defineProps<{
+  products: Product[];
+  categories: ProductCategory[];
+}>();
+</script>
+
+<template>
+  <Head title="Produkty" />
+
+  <DashboardLayout header-title="Ustawienia produktów" header-subtext="Tutaj możesz zarządzać produktami.">
+    <Table :products="products" :categories="categories" />
+  </DashboardLayout>
+</template>
