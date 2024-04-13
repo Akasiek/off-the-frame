60b60c161de07b3a8d945c24cabdf153d13e2578
Kamil Pomykała <kamil.pomykala@upware.pl>
2024-04-13 21:48:16 +0200

    feat(dashboard): enhance layout and add product forms
    # Enhances the dashboard layout and adds product management forms
    
    The changes made in this commit include:
    
    - Wrapped the dashboard layout in a `WrapperLayout` component to provide a consistent
      base layout
    - Added a `Button` component import to the `DashboardLayout`
    - Implemented a new `UpdateForm` component for editing products
    - Created a `CreateForm` component for adding new products
    - Consolidated all product form-related components under the `Form` directory
    - Updated the `BaseForm` component to handle both create and update scenarios
    
    These changes were made to improve the overall structure and functionality of the
    dashboard, making it more extensible and easier to maintain. The new product forms
    allow users to easily create and update product information directly from the
    dashboard interface.

---
+++ resources/js/Interfaces/Product.ts
@@ -0,0 +1,12 @@
+import { ProductCategory } from '@/Interfaces/ProductCategory';
+import { ProductLink } from '@/Interfaces/ProductLink';
+
+export interface Product {
+  id: number;
+  brand: string;
+  model: string;
+  product_category_id: number;
+
+  category: ProductCategory;
+  links: ProductLink[];
+}
