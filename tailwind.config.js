3850cb095b9543f5e8da28ba33ba29398370fd9c
Kamil Pomykała <kamil.pomykala@upware.pl>
2024-04-13 20:19:32 +0200

    feat: Add TypeScript support in Vue components
    Adds support for TypeScript in Vue components by updating the
    Tailwind CSS configuration to include both `.ts` and `.tsx` file
    extensions. This allows for a more robust and type-safe development
    experience when working with Vue applications.

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,7 @@ module.exports = {
     './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
     './storage/framework/views/*.php',
     './resources/views/**/*.blade.php',
-    './resources/js/**/*.vue',
+    './resources/js/**/*.{ts,tsx,vue}',
   ],
   theme: {
     container: {
