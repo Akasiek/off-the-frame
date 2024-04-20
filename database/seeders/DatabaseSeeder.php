f8cb992223cc510123d5bd18da95121dcb6b9a0c
Kamil Pomykała <kamil.pomykala@upware.pl>
2024-04-20 17:55:17 +0200

    feat: Add UserSeeder to seed admin user
    Add UserSeeder to seed an admin user using the ADMIN_EMAIL and ADMIN_PASSWORD
    values from the .env file. If these values are not set, an exception will be
    thrown.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
+            UserSeeder::class,
             ProductCategorySeeder::class,
         ]);
     }
