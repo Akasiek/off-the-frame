11b3145354c70b0fb7512e9361b5b4c9833c0125
Kamil Pomykała <kamil.pomykala@upware.pl>
2024-04-21 17:29:45 +0200

    feat: Update product table component in Dashboard.vue
    Updated the product table component in Dashboard.vue to use the ProductTable component instead of Table component. This change was made to improve naming consistency and clarity in the code.

--- resources/js/Pages/Product/Dashboard.vue
+++ resources/js/Pages/Product/Dashboard.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import DashboardLayout from '@/Layouts/DashboardLayout.vue';
 import { Head } from '@inertiajs/vue3';
-import Table from '@/Components/Product/Dashboard/Table.vue';
+import { ProductTable } from '@/Components/Product/Dashboard';
 import { Product, ProductCategory } from '@/Interfaces';
 
 defineProps<{
@@ -14,6 +14,6 @@ defineProps<{
   <Head title="Produkty" />
 
   <DashboardLayout header-title="Ustawienia produktów" header-subtext="Tutaj możesz zarządzać produktami.">
-    <Table :products="products" :categories="categories" />
+    <ProductTable :products="products" :categories="categories" />
   </DashboardLayout>
 </template>
