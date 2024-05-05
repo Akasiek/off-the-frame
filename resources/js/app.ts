c278a8830d7e8124f493d24a56c8f1e0dc53db89
Akasiek <kpomykala2002@gmail.com>
2024-05-05 21:34:12 +0200

    feat: Add GTAG

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -5,6 +5,7 @@ import { createApp, DefineComponent, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
+import VueGtag from "vue-gtag";
 
 const appName = 'Off The Frame';
 
@@ -13,6 +14,9 @@ createInertiaApp({
   resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob<DefineComponent>('./Pages/**/*.vue')),
   setup({ el, App, props, plugin }) {
     createApp({ render: () => h(App, props) })
+      .use(VueGtag, {
+        config: { id: "G-CVHEW5XT1L" }
+      })
       .use(plugin)
       .use(ZiggyVue)
       .mount(el);
