8ac2a18149eee43f24f8ac94dd1334f4b13fda71
Akasiek <kpomykala2002@gmail.com>
2024-05-06 20:21:46 +0200

    fix: ProductCategory update and delete bug

--- app/Http/Resources/ProductCategoryResource.php
+++ app/Http/Resources/ProductCategoryResource.php
@@ -13,6 +13,7 @@ class ProductCategoryResource extends JsonResource
     public function toArray($request): array
     {
         return [
+            'id' => $this->id,
             'name' => $this->name,
             'slug' => $this->slug,
         ];
