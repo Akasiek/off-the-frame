7fd8c690dab952f12a88f1b463ac58c3d708b384
Akasiek <kpomykala2002@gmail.com>
2024-07-06 19:53:55 +0200

    feat: Add caption under OutfitOfTheDay

--- resources/js/Components/OutfitOfTheDay/View/Hero.vue
+++ resources/js/Components/OutfitOfTheDay/View/Hero.vue
@@ -8,22 +8,28 @@ defineProps<{
 
 <template>
   <header class="lg:py-8 md:py-6 py-4 bg-white-0 xl:rounded-[4rem] rounded-3xl shadow-[0_0_24px_4px_#00000008]">
-    <div class="max-w-5xl my-4 mx-auto grid lg:grid-cols-[4fr_5fr] md:grid-cols-[5fr_4fr] xs:grid-cols-[6fr_4fr] grid-cols-1 gap-4">
-      <div class="lg:px-6 px-5 py-2 h-full overflow-hidden">
-        <a :href="outfitOfTheDay.image_source_url" target="_blank">
-          <img
-            :src="outfitOfTheDay.image"
-            :alt="outfitOfTheDay.image_source_url"
-            class="rounded-2xl h-full max-h-[44rem] w-full object-center object-cover"
-          />
-        </a>
+    <div class="max-w-5xl my-4 mx-auto">
+      <div class="grid lg:grid-cols-[4fr_5fr] md:grid-cols-[5fr_4fr] xs:grid-cols-[6fr_4fr] grid-cols-1 gap-4">
+        <div class="lg:px-6 px-5 py-2 h-full overflow-hidden">
+          <a :href="outfitOfTheDay.image_source_url" target="_blank">
+            <img
+              :src="outfitOfTheDay.image"
+              :alt="outfitOfTheDay.image_source_url"
+              class="rounded-2xl h-full max-h-[44rem] w-full object-center object-cover"
+            />
+          </a>
+        </div>
+
+        <div class="flex items-center justify-center">
+          <h1 class="font-serif font-medium lg:text-6xl md:text-5xl xs:text-4xl text-2xl">
+            {{ outfitOfTheDay.name }}
+          </h1>
+        </div>
       </div>
 
-      <div class="flex items-center justify-center">
-        <h1 class="font-serif font-medium lg:text-6xl md:text-5xl xs:text-4xl text-2xl">
-          {{ outfitOfTheDay.name }}
-        </h1>
-      </div>
+      <p class="font-serif md:text-sm text-xs text-gray-300 text-center mt-8 px-2">
+        If your photo has appeared on our site and you don't want it, write to us on Instagram
+      </p>
     </div>
   </header>
 </template>
